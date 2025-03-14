765073a1633e35f24b37fe99c93017d2eb6c355b
Kevin <kevinhernem@gmail.com>
2025-03-15 02:55:55 +0900

    feat: create home page component

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,5 +1,15 @@
+import FileDropzone from "../../components/FileDropzone";
+
+import "./index.css";
+
 const HomePage = () => {
-    return <h1>HomePage</h1>;
+    return (
+        <>
+            <h1 className="title">Latex Utils</h1>
+            <h2>by Kevin</h2>
+            <FileDropzone />
+        </>
+    );
 };
 
 export default HomePage;
