a29d4f4651fa4de1ac57428722ce9ec5578a28f2
Kevin <kevinhernem@gmail.com>
2025-03-17 23:31:19 +0900

    style: add styles to display and new variables

--- src/components/FileDataDisplay/index.tsx
+++ src/components/FileDataDisplay/index.tsx
@@ -12,7 +12,7 @@ const FileDataDisplay = (props: FileDataDisplayProps) => {
     return (
         <div className="file-data-display">
             <img src={CSVIcon} alt="CSV" className="csv" />
-            <div>
+            <div className="file-data-display__properties">
                 <h2>{props.data.name}</h2>
                 <p>{props.data.size}</p>
                 <p>Preview</p>
