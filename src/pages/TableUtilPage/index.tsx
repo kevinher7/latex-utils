a2ce3ff38956d765a8c01a84ff6ea4a14b127c9d
Kevin <kevinhernem@gmail.com>
2025-03-19 01:30:41 +0900

    feat: include latex display components to table utils page

--- src/pages/TableUtilPage/index.tsx
+++ src/pages/TableUtilPage/index.tsx
@@ -1,5 +1,40 @@
+import LatexCodeDisplay from "../../components/LatexCodeDisplay";
+import LatexTableDisplay from "../../components/LatexTableDisplay";
+
+import "./index.css";
+
+const c =
+    " \
+\\begin{table}[H]\n\
+    \\centering\n\
+    \\caption{I'm a caption for test.csv}\n\
+    \\label{tab:test}\n\
+    \\begin{tabular}{c|c}\n\
+        \\toprule\n\
+            1.0 & 0.2234\\\\\n\
+        \\midrule\n\
+            3 & 42.32\\\\\n\
+            1 & 2\\\\\n\
+            3 & 4\\\\\n\
+            1 & 2\\\\\n\
+            3 & 4\\\\\n\
+            1 & 2\\\\\n\
+            3 & 4\\\\\n\
+        \\bottomrule\n\
+    \\end{tabular}\n\
+\\end{table}\
+    ";
+
 const TableUtilPage = () => {
-    return <h1>Processing Result</h1>;
+    return (
+        <>
+            <h1>Processing Result</h1>
+            <section className="latex-display">
+                <LatexCodeDisplay code={c} />
+                <LatexTableDisplay />
+            </section>
+        </>
+    );
 };
 
 export default TableUtilPage;
